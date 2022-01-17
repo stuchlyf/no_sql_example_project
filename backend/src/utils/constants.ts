e71f85cc361bf9f1978b064026f401aa6e9c4656
dckjnls <dckjnls@gmail.com>
2022-01-17 14:45:33 +0100

    Add constants

---
+++ backend/src/utils/constants.ts
@@ -0,0 +1,4 @@
+export const TELEPHONE_NUMBER_REGEX = /^\D*\d{9,14}$/;
+export const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+export const STREET_NUMBER_REGEX = /\d+[a-z]*/;
+export const GENDER_REGEX = /[mwd]/;
