53653053aa4f1a4820c2affcaf0dfe25df75be0b
dckjnls <dckjnls@gmail.com>
2022-01-19 08:53:16 +0100

    Add React Router into code and create index page

---
+++ frontend/src/pages/Index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Body} from '../components/Body/Body';
+
+const Index: React.FC = () => {
+	return (
+			<Body>
+				<p>Index</p>
+			</Body>
+	)
+}
+
+export default Index;
